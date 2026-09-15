08160041e9afa2bc70185a33c9817bc16e33d7d6
Jesus <jdoninguez6@gmail.com>
2026-09-15 09:17:02 -0600

    Add photo, music, and balloon animation

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const envelope = document.getElementById("envelope");
 const scene = document.querySelector(".scene");
+const song = document.getElementById("song");
 
 envelope.addEventListener("click", () => {
   scene.classList.toggle("open");
+
+  if (scene.classList.contains("open")) {
+    song.currentTime = 0;
+
+    song.play();
+  } else {
+    song.pause();
+
+    song.currentTime = 0;
+  }
 });
